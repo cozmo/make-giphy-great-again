098fdb82c44a6277a375326581a236dc765126d5
Cosmo Wolfe <cosmo.wolfe@getclever.com>
2016-01-30 18:16:04 -0800

    Remove console.log

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(options) {
-    console.log(options)
     if(/_s\.gif/i.test(options.url)){
       return {redirectUrl: options.url.replace(/_s\.gif/i,".gif") };
     }
